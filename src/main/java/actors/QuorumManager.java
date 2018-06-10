a5ed375e077927f37ee1d8ed3852dd241585651e
Lawrence <mail@lawben.com>
2018-06-10 13:54:54 +0200

    Add basic quorum merge (#9)

--- src/main/java/actors/QuorumManager.java
+++ src/main/java/actors/QuorumManager.java
@@ -2,8 +2,13 @@ package actors;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import messages.query.QueryErrorMsg;
 import messages.query.QueryResponseMsg;
+import messages.query.QueryResultMsg;
+import messages.query.QuerySuccessMsg;
 import messages.query.TransactionMsg;
+import model.Row;
+import model.Transaction;
 import model.WriteTransaction;
 
 import java.util.ArrayList;
@@ -75,8 +80,63 @@ public class QuorumManager extends AbstractDBActor {
     }
 
     private QueryResponseMsg getQuorumResponse(List<QueryResponseMsg> messages) {
+        Transaction transaction = messages.get(0).getTransaction();
+        long transactionId = transaction.getTransactionId();
+        log.debug("Quorum for TID {}", transactionId);
+
+        Class<? extends QueryResponseMsg> msgClass = messages.get(0).getClass();
+        boolean allResponsesSameType = true;
+        QueryErrorMsg encounteredError = null;
+
+        for (QueryResponseMsg msg : messages) {
+            allResponsesSameType &= msg.getClass() == msgClass;
+            if (msg.getClass() == QueryErrorMsg.class) {
+                encounteredError = (QueryErrorMsg) msg;
+            }
+            assert msg.getTransactionId() == transactionId;
+        }
+
+        if (!allResponsesSameType) {
+            // If we have different types and no error, something is really wrong
+            assert encounteredError != null;
+
+            // We encountered an error, because they are not all errors and at least one is not of typ Success/Result
+            return new QueryErrorMsg("At least one node returned an error response: " + encounteredError.getMsg(),
+                    transaction);
+        }
+
+        if (msgClass == QueryResultMsg.class) {
+            return getResultQuorum(messages);
+        } else if (msgClass == QuerySuccessMsg.class) {
+            return getSuccessQuorum(messages);
+        } else {
+            return getErrorQuorum(messages);
+        }
+    }
+
+    private QueryResponseMsg getResultQuorum(List<QueryResponseMsg> messages) {
         // TODO: compare messages to get correct result
-        log.info("Quorum for TID {}", messages.get(0).getTransactionId());
+        Map<Long, Row> resultRowsMap = new HashMap<>();
+
+        for (QueryResponseMsg msg : messages) {
+            QueryResultMsg resultMsg = (QueryResultMsg) msg;
+            for (Row row : resultMsg.getResult()) {
+                // TODO: This will overwrite if a row is present in two different versions
+                resultRowsMap.put(row.getHashKey(), row);
+            }
+        }
+
+        List<Row> resultRows = new ArrayList<>(resultRowsMap.values());
+        return new QueryResultMsg(resultRows, messages.get(0).getTransaction());
+    }
+
+    private QueryResponseMsg getSuccessQuorum(List<QueryResponseMsg> messages) {
+        // All queries returned success, so we can return any one of them
+        return messages.get(0);
+    }
+
+    private QueryResponseMsg getErrorQuorum(List<QueryResponseMsg> messages) {
+        // All queries returned an error, so we can return any one of them
         return messages.get(0);
     }
 }
