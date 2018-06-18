fa5455e396696ea492c0528814db6a1475da5161
fawind <windheuser.fabian@gmail.com>
2018-06-18 20:19:38 +0200

    Fix packaging for ycsb benchmarks

--- benchmark/src/main/java/ActorDbClient.java
+++ benchmark/src/main/java/ActorDbClient.java
@@ -1,34 +1,45 @@
+import api.commands.CreateTableCommand;
 import api.commands.InsertIntoCommand;
-import api.commands.SelectWhereCommand;
 import api.configuration.EnvConfig;
 import client.DatastoreClient;
 import client.config.DatastoreClientModule;
 import com.google.common.collect.ImmutableList;
 import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.DB;
+import com.yahoo.ycsb.DBException;
+import com.yahoo.ycsb.Status;
 import messages.query.QueryErrorMsg;
 import messages.query.QueryResponseMsg;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import static java.lang.String.format;
+
 public class ActorDbClient extends DB {
 
     private DatastoreClient client;
 
     @Override
-    public void init() {
+    public void init() throws DBException {
         Properties props = getProperties();
-        int clientPort = Integer.valueOf(props.getProperty("clientPort", "2553"));
+        int clientPort = Integer.valueOf(props.getProperty("clientPort"));
         int storePort = Integer.valueOf(props.getProperty("storePort"));
         EnvConfig clientEnvConfig = EnvConfig.withPort(clientPort);
         EnvConfig storeEnvConfig = EnvConfig.withPort(storePort);
         client = DatastoreClientModule.createInstance(clientEnvConfig, storeEnvConfig);
         client.start();
+
+        String tableName = props.getProperty("tableName");
+        if (tableName == null || tableName.equals("null")) {
+            throw new DBException("Table name not set");
+        }
+        createDefaultTable(tableName);
     }
 
     @Override
@@ -37,23 +48,19 @@ public class ActorDbClient extends DB {
     }
 
     @Override
-    public int read(String table, String key, Set<String> fields, HashMap<String, ByteIterator> result) {
-        SelectWhereCommand cmd = SelectWhereCommand.builder()
-                .tableName(table)
-                .whereFn(row -> row.getKey().equals(key))
-                .build();
-        CompletableFuture<QueryResponseMsg> response = client.sendRequest(cmd);
-        return checkQuerySuccess(response);
+    public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+        // TODO: Implement where fn
+        return Status.OK;
     }
 
     @Override
-    public int scan(String table, String startKey, int recordcount, Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
+    public Status scan(String table, String startKey, int recordcount, Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
         // TODO: Implement range queries
-        return Status.SUCCESS;
+        return Status.OK;
     }
 
     @Override
-    public int update(String table, String key, HashMap<String, ByteIterator> values) {
+    public Status update(String table, String key, Map<String, ByteIterator> values) {
         InsertIntoCommand cmd = InsertIntoCommand.builder()
                 .tableName(table)
                 .values(ImmutableList.of(key, values.toString()))
@@ -63,7 +70,7 @@ public class ActorDbClient extends DB {
     }
 
     @Override
-    public int insert(String table, String key, HashMap<String, ByteIterator> values) {
+    public Status insert(String table, String key, Map<String, ByteIterator> values) {
         InsertIntoCommand cmd = InsertIntoCommand.builder()
                 .tableName(table)
                 .values(ImmutableList.of(key, values.toString()))
@@ -73,7 +80,7 @@ public class ActorDbClient extends DB {
     }
 
     @Override
-    public int delete(String table, String key) {
+    public Status delete(String table, String key) {
         // TODO: Implement delete
         InsertIntoCommand cmd = InsertIntoCommand.builder()
                 .tableName(table)
@@ -83,16 +90,28 @@ public class ActorDbClient extends DB {
         return checkQuerySuccess(response);
     }
 
-    private int checkQuerySuccess(CompletableFuture<QueryResponseMsg> response) {
+    private Status checkQuerySuccess(CompletableFuture<QueryResponseMsg> response) {
         try {
             QueryResponseMsg msg = response.get();
             if (msg instanceof QueryErrorMsg) {
                 return Status.ERROR;
             }
-            return Status.SUCCESS;
+            return Status.OK;
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
             return Status.ERROR;
+        }
+    }
+
+    private void createDefaultTable(String tableName) throws DBException {
+        CreateTableCommand cmd = CreateTableCommand.builder()
+                .tableName(tableName)
+                .schema(ImmutableList.of("string", "string", "string"))
+                .build();
+        CompletableFuture<QueryResponseMsg> response = client.sendRequest(cmd);
+        Status status = checkQuerySuccess(response);
+        if (status == Status.ERROR) {
+            throw new DBException(format("Error creating default table: %s", tableName));
         }
     }
 }
--- benchmark/src/main/java/Status.java
+++
@@ -1,7 +0,0 @@
-public class Status {
-
-    public static int SUCCESS = 0;
-    public static int ERROR = 1;
-
-    private Status() {}
-}
