b1df95d2391c99ac6ff8d1131482e9bf4d77f679
fawind <windheuser.fabian@gmail.com>
2018-09-08 14:18:20 -0400

    Mod benchmark size

--- benchmark/scripts/run.sh
+++ benchmark/scripts/run.sh
@@ -12,7 +12,7 @@ java \
   -threads 16 \
   -db ActorDbClient \
   -P ./workloads/workloadc \
-  -P ./500k.dat \
+  -P ./300k.dat \
   -p clientPort=0 \
   -p storeIp="206.189.238.25" \
   -p storePort=2552 \
