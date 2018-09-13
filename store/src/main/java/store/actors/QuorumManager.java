d844d3f073dd9398bfba68e35e21d5ce86c065ab
Lawrence <mail@lawben.com>
2018-09-13 19:28:10 -0400

    Add QM sutff

--- store/src/main/java/store/actors/QuorumManager.java
+++ store/src/main/java/store/actors/QuorumManager.java
@@ -3,11 +3,19 @@ package store.actors;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import api.messages.LamportId;
+import api.messages.QueryErrorMsg;
 import api.messages.QueryMetaInfo;
 import api.messages.QueryMsg;
+import api.messages.QueryResponseMsg;
+import api.messages.QueryResultMsg;
+import api.model.Row;
 import store.configuration.DatastoreConfig;
 import store.configuration.DatastoreModule;
+import store.messages.query.PartialQueryResultMsg;
+import store.model.StoredRow;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -36,6 +44,7 @@ public class QuorumManager extends AbstractDBActor {
     public Receive createReceive() {
         return receiveBuilder()
                 .match(QueryMsg.class, this::handleQuorum)
+                .match(QueryResponseMsg.class, this::handleQueryResponseMsg)
                 .matchAny(x -> log.error("Unknown message: {}", x))
                 .build();
     }
@@ -48,7 +57,8 @@ public class QuorumManager extends AbstractDBActor {
         ActorRef quorumCollector = getContext().actorOf(QuorumResponseCollector.props(msg.getRequester(), quorumSize));
 
         memberRegistry.getMasters()
-                .forEach(actorPath -> getContext().actorSelection(actorPath).tell(msg, quorumCollector));
+//                .forEach(actorPath -> getContext().actorSelection(actorPath).tell(msg, quorumCollector));
+                .forEach(actorPath -> getContext().actorSelection(actorPath).tell(msg, getSelf()));
     }
 
     private QueryMetaInfo addNewLamportId(QueryMetaInfo queryMetaInfo) {
@@ -58,5 +68,36 @@ public class QuorumManager extends AbstractDBActor {
     private LamportId updatedLamportId(LamportId other) {
         lamportId = lamportId.maxIdCopy(other);
         return lamportId;
+    }
+
+    private void handleQueryResponseMsg(QueryResponseMsg msg) {
+        lamportId = lamportId.max(msg.getLamportId());
+
+        // For now, if we see an error we assume everything is an error
+        if (msg instanceof QueryErrorMsg) {
+            tellWithNewestResponseLamportId(msg);
+            return;
+        }
+
+        // Seen all results, pass result to quorumManager
+//        QueryResponseMsg quorumResponse = getQuorumResponse();
+        PartialQueryResultMsg resultMsg = (PartialQueryResultMsg) msg;
+        List<StoredRow> storedRows = resultMsg.getResult();
+        List<Row> results = new ArrayList<>();
+        for (StoredRow sr : storedRows) {
+            results.add(sr.getRow());
+        }
+
+        QueryResponseMsg quorumResponse = new QueryResultMsg(results, msg.getQueryMetaInfo());
+        tellWithNewestResponseLamportId(quorumResponse);
+    }
+
+    private void tellWithNewestResponseLamportId(QueryMsg msg) {
+        msg.updateMetaInfo(addResponseLamportIdToMeta(msg.getQueryMetaInfo()));
+        msg.getQueryMetaInfo().getRequester().tell(msg, ActorRef.noSender());
+    }
+
+    private QueryMetaInfo addResponseLamportIdToMeta(QueryMetaInfo metaInfo) {
+        return metaInfo.copyWithResponseLamportId(updatedLamportId(metaInfo.getLamportId()));
     }
 }
